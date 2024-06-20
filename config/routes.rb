5cbc1f57de9f5f63c123de8b47074e0efbc66489
AliakseiRabushka <ljo6e4kin@gmail.com>
2024-06-20 22:02:30 +0300

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'boards#index'
 
+  resources :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
   resources :boards do
     resources :tasks
   end
